417675fff8a20f6f7df91c7e4da77cae02da2eab
zettlrobert <zettl.robert@gmail.com>
2023-02-16 19:15:23 +0100

    feat: add execution path

--- pop/setup.sh
+++ pop/setup.sh
@@ -11,6 +11,8 @@ export lightgreen='\x1b[1;32m'
 export lightgray='\x1b[1;37m'
 export NC='\x1b[0m'
 
+DIR="${BASH_SOURCE%/*}"
+
 function verify_command {
 	message=$1
 	exitCode=$2
@@ -56,7 +58,7 @@ function remove_base_directories {
 	cd "$HOME" || exit
 
 	# Delete all directgories
-	rm -r Documents Music Public Videos Desktop Downloads Pictures Templates
+	rm -r "$HOME"/Documents "$HOME"/Music "$HOME"/Public "$HOME"/Videos "$HOME"/Desktop "$HOME"/Downloads "$HOME"/Pictures "$HOME"/Templates
 
 	# Clone this repository
 	git clone https://github.com/zettlrobert/configurations.git
@@ -65,7 +67,7 @@ function remove_base_directories {
 function create_new_directories {
 	# Create new directory for every string in array
 	for directory in "${DIRECTORIES[@]}"; do
-		mkdir $directory
+		mkdir "$directory"
 		exitCode=$?
 
 		verify_command "creating $directory" $exitCode
@@ -79,7 +81,7 @@ function update_xdg_user_dirs {
 	verify_command "removing user-dirs.dirs" $exitCodeRmUserDirs
 
 	# Copy user.user-dirs.dirs from assets
-	cp ./assets/user-dirs.dirs "$HOME"/.config
+	cp "$DIR"/assets/user-dirs.dirs "$HOME"/.config
 }
 
 remove_base_directories
@@ -89,7 +91,7 @@ update_xdg_user_dirs
 echo -e "Tasks\n\
   - Switch Keyboard Shurtcuts, (super for workspace overview)\n\
   - Update github ssh keys with: .ssh/id_rsa.pub
-  - 
+  - Adjust download folder in tools(browser) to downloads instead of Downloads
 "
 
 # TODO:
